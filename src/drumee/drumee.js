b0b9310cb3655eb4540c7e4aa9bbb776c475d798
Somanos Sar <somanos@drumee.com>
2025-09-13 09:45:37 +0200

    Changed initial page language selector

--- src/drumee/drumee.js
+++ src/drumee/drumee.js
@@ -130,10 +130,9 @@ class Drumee extends Marionette.Application {
       Organization.set(organization)
       window.currentDevice = Visitor.device();
       window.SERVICE = Platform.get('services')
-      if(_.isEmpty(SERVICE)){
+      if (_.isEmpty(SERVICE)) {
         window.SERVICE = require('lex/services');
       }
-      console.log('AAAA:133', SERVICE)
     } catch (e) {
       console.error("FAILED TO PARSE ENVIRONMENT DATA", e);
       this.failover(e);
@@ -148,9 +147,8 @@ class Drumee extends Marionette.Application {
     let { user } = data;
     let { lang } = user;
     if (!lang) {
-      lang = Visitor.pagelang();
+      lang = Visitor.language();
     }
-    console.log(`Using locale language=${lang}`)
     require.ensure(["application"], e => {
       window.LOCALE = require("locale")(lang);
       if (_.isFunction(locale)) {
@@ -167,7 +165,9 @@ class Drumee extends Marionette.Application {
       this.showView(this.router);
       Visitor.listenChanges();
       Organization.listenChanges();
-      Visitor.respawn(user);
+      if (user.id) {
+        Visitor.respawn(user);
+      }
       if (!Backbone.History.started) Backbone.history.start();
     });
   }
