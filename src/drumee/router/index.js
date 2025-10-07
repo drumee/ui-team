e45f73ff0e46d1bdbfb56d839d5f553c597e3f99
Somanos Sar <somanos@drumee.com>
2025-10-07 17:11:42 +0200

    Migration WIP

--- src/drumee/router/index.js
+++ src/drumee/router/index.js
@@ -8,7 +8,7 @@ require("./skin/resizable.css");
 require("builtins/contextmenu/skin/index.scss");
 // require("jquery-ui/ui/core");
 // require("jquery-ui/ui/widget");
-require("skin/global/common");
+require("skin");
 require("skin/vars/index.scss");
 const { appendFontFace, appendLink } = require("core/utils");
 const { getModule, moduleName } = require('./modules');
