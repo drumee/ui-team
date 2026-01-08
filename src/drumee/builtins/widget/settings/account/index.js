b6ca4027396dec2d8e41f782d572b21df155beea
Somanos Sar <somanos@drumee.com>
2026-01-08 22:19:48 +0100

    3.2.4

--- src/drumee/builtins/widget/settings/account/index.js
+++ src/drumee/builtins/widget/settings/account/index.js
@@ -257,29 +257,16 @@ class settings_account extends LetcBox {
 * 
 */
   _openLink(url) {
-    // Best practice for iOS/iPadOS Safari
-    function openInNewTab(url) {
-      const link = document.createElement('a');
-      link.href = url;
-      link.target = '_blank';
-      link.rel = 'noopener noreferrer';
-      if (Visitor.device() == _a.desktop) {
-        let w = Math.min(900, screen.availWidth - 100);
-        let h = Math.min(700, screen.availHeight - 100);
-        let x = screen.availWidth / 2 - w / 2;
-        let y = 0;
-        link.rel = `noopener, noreferrer, width=${w}, height=${h}, left=${x}, top=${y}`;
-      }
-      document.body.appendChild(link);
-      link.click();
-      setTimeout(() => {
-        document.body.removeChild(link);
-      }, 500)
-    }
-
-    // Usage
-    openInNewTab(url);
     this.__overlay.softClear()
+    if (Visitor.device() == _a.mobile) {
+      window.open(url, "_blank", "noopener; noreferrer");
+    } else {
+      let w = Math.min(900, screen.availWidth - 100);
+      let h = Math.min(700, screen.availHeight - 100);
+      let x = screen.availWidth / 2 - w / 2;
+      let y = 0;
+      window.open(url, "_blank", `noopener, noreferrer, width=${w}, height=${h}, left=${x}, top=${y}`);
+    }
   }
 
   /**
@@ -323,9 +310,8 @@ class settings_account extends LetcBox {
         return this.ensurePart("avatar-widget").then((p) => {
           p.selectFile();
         });
-      // case "open-payment-link":
-      case "proceed-to-payment":
-        this._openLink(args.url)
+      case "open-payment-link":
+        this._openLink(cmd.mget(_a.value))
         const onVisibilityChange = () => {
           if (document.hidden) return
           this._onPlanChanged()
@@ -337,11 +323,11 @@ class settings_account extends LetcBox {
         return this.ensurePart("avatar-progress").then((p) => {
           p.el.style.width = `${args.progress}%`;
         });
-      // case "proceed-to-payment":
-      //   return this.__overlay.feed(
-      //     require("./skeleton/payment").default(this, args.url)
-      //   );
-      //   break;
+      case "proceed-to-payment":
+        return this.__overlay.feed(
+          require("./skeleton/payment").default(this, args.url)
+        );
+        break;
       case 'plan_updated':
         this._onPlanChanged(cmd)
         break;
