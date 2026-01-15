7ca3230614afa5a5d8d5450d16f4c7c073e9328f
Drumee Dev <drumee@debian.local.drumee>
2026-01-15 01:09:26 -0800

    add responsive for sign up page

--- src/drumee/modules/welcome/signin/skeleton/index.js
+++ src/drumee/modules/welcome/signin/skeleton/index.js
@@ -1,32 +1,42 @@
-function __skl_welcome_signin (_ui_, opt, type="") {
-  const fig = _ui_.fig.family
+function __skl_welcome_signin(_ui_, opt, type = "") {
+  const fig = _ui_.fig.family;
   let dataset = _ui_.mget(_a.dataset) || {};
+  const logo = Skeletons.Box.X({
+    active: 0,
+    className: `${fig}__logo-content`,
+    kids: [
+      Skeletons.Button.Svg({
+        ico: "raw-logo-drumee-full",
+        lassName: `${fig}__logo-icon`,
+      }),
+    ],
+  });
   let a = Skeletons.Box.Y({
-    className  : `${fig}__main`,
-    debug      : __filename,
-    kids       : [
+    className: `${fig}__main`,
+    debug: __filename,
+    kids: [
       Skeletons.Box.Y({
-        className  : `${fig}__container`,
+        className: `${fig}__container`,
         dataset,
-        kids : [
+        kids: [
           Skeletons.Box.Y({
-            className  : `${fig}__header`,
-            sys_pn     : _a.header,
+            className: `${fig}__header`,
+            sys_pn: _a.header,
             dataset,
-            kids : [opt.header]
+            kids: [opt.header],
           }),
+          logo,
           Skeletons.Box.Y({
-            className  : `${fig}__content ${type}`,
-            sys_pn     : _a.content,
-            kids : [opt.content]
-          })
-        ]
-      })
-    ]
-  })
+            className: `${fig}__content ${type}`,
+            sys_pn: _a.content,
+            kids: [opt.content],
+          }),
+        ],
+      }),
+    ],
+  });
 
   return a;
-
-};
+}
 
 module.exports = __skl_welcome_signin;
