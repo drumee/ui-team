7f505b06426f4cb1e4c15e38127e0fdc238e7e65
Somanos Sar <somanos@drumee.com>
2026-02-07 16:35:49 +0100

    changed pages/member

---
+++ src/drumee/builtins/window/adminpanel/pages/members/skeleton/action-popup/content/kick-member.js
@@ -0,0 +1,38 @@
+export default function (ui) {
+  const contentFig = `${ui.fig.family}-action-popup-confirmation`;
+
+  const header = Skeletons.Note({
+    className: `${contentFig}__note header`,
+    content: "Really kick this member out"
+  });
+
+  const subHeader = Skeletons.Note({
+    className: `${contentFig}__note sub-header`,
+    content: `Content belonging to your organization will be removed from the member.
+    However personal content available to the member.`
+  });
+
+  const profileDisplay = require('../profile-display').default(ui, 'no-sub-content')
+
+  const buttons = Preset.ConfirmButtons(ui, {
+    cancelLabel: LOCALE.CANCEL || '',
+    cancelService: 'close-overlay',
+    confirmLabel: LOCALE.KICK_OUT_MEMBER || '',
+    confirmService: 'confirm-kick-out',
+    confirmBtnAction: 'destroy'
+  });
+
+
+  const a = Skeletons.Box.Y({
+    debug: __filename,
+    className: `${contentFig}__content delete-member`,
+    kids: [
+      header,
+      subHeader,
+      profileDisplay,
+      buttons
+    ]
+  });
+
+  return a;
+};
