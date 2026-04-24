a6089094d9ed85a1a5c8a8e387006f79f53673e6
Drumee Dev <drumee@debian.local.drumee>
2026-04-24 05:10:53 -0700

    add more dropdown button of media grid

--- src/drumee/builtins/media/grid/index.js
+++ src/drumee/builtins/media/grid/index.js
@@ -81,10 +81,58 @@ class __media_grid extends DrumeeMediaInteract {
         })
       )
     }
+
+    if (filetype !== _a.hub) {
+      this.container.push(require('./template/context-menu')(this));
+    }
   }
 
   /**
-   * 
+   * @param {Event} e
+   */
+  dispatchUiEvent(e) {
+    const service = this.el.getService(e);
+    if (service === 'context-menu') {
+      e.stopPropagation();
+      this.ensurePart('context-menu').then((menu) => {
+        const opening = menu.el.dataset.state !== '1';
+        if (opening) {
+          const trigger = this.el.querySelector('.media-context-menu__trigger');
+          if (trigger) {
+            const rect = trigger.getBoundingClientRect();
+            const menuW = 168;
+            menu.el.style.top = `${rect.bottom + 4}px`;
+            menu.el.style.left = `${Math.max(4, rect.right - menuW)}px`;
+          }
+          menu.el.dataset.state = '1';
+          const closeOnOutside = (evt) => {
+            if (!this.el.contains(evt.target)) {
+              menu.el.dataset.state = '0';
+              document.removeEventListener('click', closeOnOutside, true);
+            }
+          };
+          document.addEventListener('click', closeOnOutside, true);
+        } else {
+          menu.el.dataset.state = '0';
+        }
+      });
+      return;
+    }
+    super.dispatchUiEvent(e);
+  }
+
+  /**
+   * @param {*} cmd
+   * @param {*} args
+   */
+  onUiEvent(cmd, args = {}) {
+    const menu = this.getPart('context-menu');
+    if (menu) menu.el.dataset.state = '0';
+    super.onUiEvent(cmd, args);
+  }
+
+  /**
+   *
    */
   rowsCount(value) {
     let l = 1;
